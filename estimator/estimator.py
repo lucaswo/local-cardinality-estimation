2bf236c22265307fa00064fe65094e1e0dc6680e
Andreas Geyer <andreas.geyer@tu-dresden.de>
2020-04-28 18:25:32 +0200

    - added config.yaml to easily customize the neural network - rework of get_model with usage of config

--- estimator/estimator.py
+++ estimator/estimator.py
@@ -1,4 +1,7 @@
+from typing import Dict, Any
+
 import numpy as np
+import yaml
 from keras import backend as K
 from keras.layers import Dense, Input, Dropout
 from keras.models import Model
@@ -8,29 +11,57 @@ from keras.optimizers import Adam
 class Estimator:
     max_card_log: int = None
 
+    config: dict = None
+    data_x = None
+    data_y = None
+
     model: Model = None
 
-    def __init__(self):
-        print("Placeholder")
+    def __init__(self, config: Dict[str, Any] = None, data_x=None, data_y=None, model: Model = None, debug: bool = True):
 
-    @staticmethod
-    def get_model(width: int = 64, depth: int = 2, loss: str = "mean_squared_error", len_input: int = 32,
-                  dropout: float = 0.2, learning_rate: float = 0.0001, kernel_initializer: str = "normal"):
+        if config is None:
+            with open("config.yaml") as file:
+                config = yaml.safe_load(file)
+
+        conf_keys = ["width", "loss_function", "dropout", "learning_rate", "kernel_initializer", "activation_strategy"]
+
+        for key in conf_keys:
+            if config[key] is None or config[key] == "":
+                raise ValueError(
+                    "Value for {} is needed! You can provide it with the config dict or in config.yaml!".format(key))
+
+        if config["layer"] is None or config["layer"] == "" or len(config["layer"]) < 1:
+            raise ValueError("Value for layer is needed! You can provide it with the config dict or in config.yaml!")
+
+        self.config = config
+
+        self.data_x = data_x
+        self.data_y = data_y
+
+        self.model = model
+
+        if debug:
+            print("Initialized Estimator with {}".format(config))
+
+    def get_model(self, len_input: int = 32) -> Model:
 
         one_input = Input(shape=(len_input,), name='one_input')
-        x = Dense(width, activation="relu", kernel_initializer=kernel_initializer)(one_input)
 
-        for i in range(1, depth):
-            width = max(8, int(width / 2))
-            x = Dense(width, activation="relu", kernel_initializer=kernel_initializer)(x)
-            x = Dropout(dropout)(x)
+        for index, width in enumerate(self.config["layer"]):
+            if width is None or width < 0:
+                raise ValueError("A value provided for the layer is not set or negative! You can correct it in the "
+                                 "config dict or in config.yaml!")
 
-        x = Dense(1, kernel_initializer=kernel_initializer, name="main_output", activation="linear")(x)
+            x = Dense(width, activation="relu", kernel_initializer=self.config["kernel_initializer"])(
+                one_input if index == 0 else x)
+            x = Dropout(rate=self.config["dropout"])(x)
 
-        model = Model(inputs=one_input, outputs=x)
-        model.compile(loss=loss, optimizer=Adam(lr=learning_rate))
+        x = Dense(1, kernel_initializer=self.config["kernel_initializer"], name="main_output", activation="linear")(x)
 
-        return model
+        self.model = Model(inputs=one_input, outputs=x)
+        self.model.compile(loss=self.config["loss_function"], optimizer=Adam(lr=self.config["learning_rate"]))
+
+        return self.model
 
     @staticmethod
     def denormalize(y, y_min, y_max):
@@ -58,3 +89,19 @@ class Estimator:
         y_pred = self.denormalize_np(y_pred, 0, self.max_card_log)
 
         return np.maximum(y_true, y_pred) / np.minimum(y_true, y_pred)
+
+    def split_data(self):
+        print()
+
+    def train(self):
+        print()
+
+    def test(self):
+        print()
+
+    def evaluate(self):
+        print()
+
+
+est = Estimator()
+est.get_model()
