736f634e89e0ae2b49cf180521ab1f47d96e7ffc
Andreas Geyer <andreas.geyer@tu-dresden.de>
2020-04-30 19:47:36 +0200

    - estimator is working now - added requirements.txt

--- estimator/estimator.py
+++ estimator/estimator.py
@@ -10,9 +10,12 @@ from keras.optimizers import Adam
 
 class Estimator:
     max_card: float = None
+    input_length: int = None
 
     config: Dict = None
-    data: Dict[str, np.ndarray] = {"x": None, "y": None}
+    data: Dict[str, np.ndarray] = {"x": None, "y": None, "postgres_estimate": None}
+    training_data: Dict[str, np.ndarray] = {"x": None, "y": None, "postgres_estimate": None}
+    test_data: Dict[str, np.ndarray] = {"x": None, "y": None, "postgres_estimate": None}
 
     model: Model = None
 
@@ -23,7 +26,7 @@ class Estimator:
             with open("config.yaml") as file:
                 config = yaml.safe_load(file)
 
-        conf_keys = ["width", "loss_function", "dropout", "learning_rate", "kernel_initializer", "activation_strategy"]
+        conf_keys = ["loss_function", "dropout", "learning_rate", "kernel_initializer", "activation_strategy"]
 
         for key in conf_keys:
             if config[key] is None or config[key] == "":
@@ -40,7 +43,10 @@ class Estimator:
         if debug:
             print("Initialized Estimator with {}".format(config))
 
-    def get_model(self, len_input: int = 32) -> Model:
+    def get_model(self, len_input: int) -> Model:
+        if not len_input:
+            raise ValueError("You should load the data before creating a model, because the model needs to know the "
+                             "size of the input-vector!")
 
         one_input = Input(shape=(len_input,), name='one_input')
 
@@ -115,20 +121,74 @@ class Estimator:
 
         self.max_card = np.log(np.max(self.data["y"]))
 
+        self.input_length = len(self.data["x"][0])
+
         self.data["y"] = self.normalize(self.data["y"])
 
-    def split_data(self):
-        print()
+    def split_data(self, split: float = 0.9):
+        if "x" not in self.data or self.data["x"] is None:
+            raise KeyError("You're trying to split data you haven't even loaded! Be sure to load data before attempting"
+                           " to split it!")
 
-    def train(self):
-        print()
+        sample = np.random.choice(range(len(self.data["x"])), size=int(len(self.data["x"]) * split), replace=False)
+        not_sample = list(set(range(len(self.data["x"]))) - set(sample))
+
+        self.training_data["x"] = self.data["x"][sample]
+        self.training_data["y"] = self.data["y"][sample]
+        if "postgres_estimate" in self.data and self.data["postgres_estimate"]:
+            self.training_data["postgres_estimate"] = self.data["postgres_estimate"][sample]
+
+        self.test_data["x"] = self.data["x"][not_sample]
+        self.test_data["y"] = self.data["y"][not_sample]
+        if "postgres_estimate" in self.data and self.data["postgres_estimate"]:
+            self.test_data["postgres_estimate"] = self.data["postgres_estimate"][not_sample]
+
+    def train(self, epochs: int = 100, verbose: int = 0, shuffle: bool = True, batch_size: int = 32,
+              validation_split: float = 0.1):
+        if "x" not in self.training_data or self.training_data["x"] is None or "y" not in self.training_data or \
+                self.training_data["y"] is None:
+            raise KeyError("You're trying to train the network without loading data! Be sure to load data before "
+                           "attempting to train the neural network!")
+        if not self.model:
+            raise ValueError("You haven't built a model to train yet.")
+
+        return self.model.fit(self.training_data["x"], self.training_data["y"], epochs=epochs, verbose=verbose,
+                              shuffle=shuffle, batch_size=batch_size, validation_split=validation_split)
 
     def test(self):
-        print()
+        if "x" not in self.test_data or self.test_data["x"] is None or "y" not in self.test_data or \
+                self.test_data["y"] is None:
+            raise KeyError("You're trying to test the network without loading data! Be sure to load data before "
+                           "attempting to test the neural network!")
+        if not self.model:
+            raise ValueError("You haven't built a model to test yet.")
 
-    def evaluate(self):
-        print()
+        return self.model.predict(self.test_data["x"])
 
+    def predict(self, data: np.ndarray):
+        if not self.model:
+            raise ValueError("You haven't built a model to predict with, yet.")
+
+        return self.model.predict(data)
+
+    def run(self, data_file_path: str, epochs: int = 100, verbose: int = 1, shuffle: bool = True, batch_size: int = 32,
+            validation_split: float = 0.1, save_history: bool = True):
+        self.load_data_file(data_file_path)
+        self.split_data()
+        self.get_model(self.input_length)
+        history = self.train(epochs=epochs, verbose=verbose, shuffle=shuffle, batch_size=batch_size,
+                             validation_split=validation_split)
+        predictions = self.test()
+
+        if save_history:
+            self.save_history(history.history)
+
+        q_error_means = [np.mean(self.q_loss_np(self.test_data["y"], predictions[:, 0]))]
+
+        return q_error_means
+
+
+# TODO: add documentation, add safety-features, add save-methods
 
 est = Estimator()
-est.load_data_file("vectors_census.csv")
+est.run("vectors_census.csv", epochs=1)
