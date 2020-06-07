c3f40e339dedcc388b8ab488209e8eb1b01856a5
Thomas <thomas.goerlach@tu-dresden.de>
2020-06-07 14:38:23 +0200

    #7: polishing: outsourced query import in own function

--- postgres_evaluator/postgres_evaluator.py
+++ postgres_evaluator/postgres_evaluator.py
@@ -2,11 +2,11 @@ from typing import List, Tuple, Dict
 
 import psycopg2 as postgres
 import yaml
-
+import csv
 
 # TODO: Dokumentation für SQL aus CSV/SQL-file laden ergänzen
 # TODO: gegen Fehler absichern, entsprechende Meldungen machen
-# TODO: hardgecodete Filenamen ersetzen, CSV-Varianten ergänzen
+# TODO: CSV-Varianten ergänzen
 
 class PostgresEvaluator:
     """
@@ -25,7 +25,7 @@ class PostgresEvaluator:
 
     debug: bool = None
 
-    def __init__(self, config: dict = None, debug: bool = True, sql_file: str = 'queries.sql'):
+    def __init__(self, config: dict = None, debug: bool = True, input_file_name: str = 'queries.sql'):
         """
         Initializer for the PostgresEvaluator
 
@@ -37,7 +37,7 @@ class PostgresEvaluator:
             and port (default to host: localhost, port: 5432 if not given)
             if not given: the config file 'config.yaml' is used for these settings
         :param debug: boolean whether to print additional information while processing
-        :param sql_file: name of the file used for the sql query import
+        :param input_file_name: name of the file used for the sql query import, have to be .csv or .sql and located in the asset folder
         """
 
         if config is None:
@@ -59,15 +59,10 @@ class PostgresEvaluator:
 
         self.debug = debug
 
-        with open(sql_file, 'r') as f:
-            sql_file = f.read()
-        self.sql_queries = list(filter(None, sql_file.split('\n')))
+        path = "../assets/" + input_file_name
+        self.sql_queries = self.import_sql_queries(path)
+        self.explain_queries = self.generate_explain_queries()
 
-        self.explain_queries = []
-        for query in self.sql_queries:
-            tmp = query.split('COUNT(*)')
-            explain_query = "EXPLAIN " + tmp[0] + "*" + tmp[1]
-            self.explain_queries.append(explain_query)
 
     def open_database_connection(self):
         """
@@ -100,6 +95,30 @@ class PostgresEvaluator:
             if self.debug:
                 print("Closing connection to postgres")
             self.conn.close()
+
+    def import_sql_queries(self, path):
+        sql_queries = []
+        if path.endswith(".sql"):
+            with open(path, 'r') as f:
+                input_file = f.read()
+                sql_queries = list(filter(None, input_file.split('\n')))
+                if self.debug:
+                    print("loaded queries from ", )
+        elif path.endswith(".csv"):
+            sql_queries = []
+        else:
+            print("There was no sql or csv file to import the queries found, no further processing possible.")
+            sql_queries = []
+
+        return sql_queries
+
+    def generate_explain_queries(self):
+        explain_queries = []
+        for query in self.sql_queries:
+            tmp = query.split('COUNT(*)')
+            explain_query = "EXPLAIN " + tmp[0] + "*" + tmp[1]
+            explain_queries.append(explain_query)
+        return explain_queries
 
     def get_true_cardinalities(self):
         cardinalities = []
@@ -137,7 +156,7 @@ class PostgresEvaluator:
             if self.debug:
                 print(
                     "Save queries and corresponing cardinalities (firstly estimated, secondly true) on 'queries_with_cardinalities.txt'")
-            for idx, q in enumerate(self.sql_cueries):
+            for idx, q in enumerate(self.sql_queries):
                 entry = q + ' ' + str(ec[idx]) + ' ' + str(tc[idx]) + '\n'
                 f.write(entry)
 
