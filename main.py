da1720a2b0501d36e3f901b9269a0f87683224de
lllAlexanderlll <alex.a.weber@gmx.de>
2020-06-08 12:34:58 +0200

    added main.py function for vectorizer

---
+++ main.py
@@ -0,0 +1,9 @@
+from vectorizer import Vectorizer
+
+def vectorize(queries_with_cardinalities_csv_path : str, ouput_folder_path : str, filename : str):
+    vectorizer = Vectorizer(4)
+    vectorizer.add_queries_with_cardinalities(queries_with_cardinalities_csv_path)
+    vectorizer.vectorize()
+    vectorizer.save(ouput_folder_path, filename)
+
+vectorize("assets/queries_with_cardinalities.csv", "/mnt/data/programming/tmp/", "main_py_test")
