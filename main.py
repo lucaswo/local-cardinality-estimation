c6f62ac5a33b1f8427de847fcf5f3a917ed4f77a
Alexander Arno Weber <alexander_arno.weber@tu-dresden.de>
2020-06-13 18:46:45 +0200

    Apply suggestion to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from vectorizer import Vectorizer
 
-def vectorize(queries_with_cardinalities_csv_path : str, ouput_folder_path : str, filename : str):
+def vectorize(queries_with_cardinalities_csv_path : str, output_folder_path : str, filename : str):
     vectorizer = Vectorizer(4)
     vectorizer.add_queries_with_cardinalities(queries_with_cardinalities_csv_path)
     vectorizer.vectorize()
