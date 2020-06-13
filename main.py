805587b5ee06d9696880c21bfcc2a461bde1fa79
Alexander Arno Weber <alexander_arno.weber@tu-dresden.de>
2020-06-13 18:46:55 +0200

    Apply suggestion to main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def vectorize(queries_with_cardinalities_csv_path : str, output_folder_path : st
     vectorizer = Vectorizer(4)
     vectorizer.add_queries_with_cardinalities(queries_with_cardinalities_csv_path)
     vectorizer.vectorize()
-    vectorizer.save(ouput_folder_path, filename)
+    vectorizer.save(output_folder_path, filename)
 
 vectorize("assets/queries_with_cardinalities.csv", "/mnt/data/programming/tmp/", "main_py_test")
